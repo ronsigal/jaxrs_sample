ee84ebc4667452f07dac4c3729dd7e5545e84b49
Ron Sigal <rsigal@redhat.com>
2021-09-17 11:52:48 -0400

    Adding TestApp.java

---
+++ src/main/java/org/jboss/resteasy/test/grpc/TestApp.java
@@ -0,0 +1,9 @@
+package org.jboss.resteasy.test.grpc;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("root")
+public class TestApp extends Application {
+	
+} 
